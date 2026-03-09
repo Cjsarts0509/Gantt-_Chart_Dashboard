3f16822e82139290a0efda5b749b0740d3c184fd
Cjsarts0509 <4rumarts@gmail.com>
2026-03-09 19:33:33 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Gantt _Chart_Dashboard/',
+  base: '/Gantt-_Chart_Dashboard/',
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
